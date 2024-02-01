6289efca679f1f347aa31ba6f8a4ed6b991e640d
Anderson Braz <contato@andersonbraz.com>
2024-01-31 22:07:50 -0300

    Up Files(s).

---
+++ sql/sales.sql
@@ -0,0 +1,14 @@
+CREATE TABLE sales (
+    OrderID TEXT PRIMARY KEY,
+    Quantity INTEGER,
+    UnitPrice INTEGER,
+    Status INTEGER,
+    OrderDate DATE,
+    Product_Category TEXT,
+    Sales_Manager TEXT,
+    Shipping_Cost INTEGER,
+    Delivery_Time INTEGER,
+    Shipping_Address TEXT,
+    Product_Code TEXT,
+    OrderCode INTEGER
+);
